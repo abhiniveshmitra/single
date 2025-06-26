13c27f9082ca3d7b0baa1ee1d01ca27f6bb23091
abhiniveshmitra <133777817+abhiniveshmitra@users.noreply.github.com>
2025-06-26 12:15:26 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,1 +1,35 @@
-az cognitiveservices model list --location westeurope --query "[?contains(model.name, 'gpt-4')]"
+{
+  "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {
+    "accountName": {
+      "type": "string",
+      "defaultValue": "eus-cdr-openai"
+    },
+    "location": {
+      "type": "string",
+      "defaultValue": "westeurope"
+    },
+    "skuName": {
+      "type": "string",
+      "defaultValue": "S0"
+    }
+  },
+  "resources": [
+    {
+      "type": "Microsoft.CognitiveServices/accounts",
+      "apiVersion": "2023-05-01",
+      "name": "[parameters('accountName')]",
+      "location": "[parameters('location')]",
+      "sku": {
+        "name": "[parameters('skuName')]"
+      },
+      "kind": "OpenAI",
+      "properties": {
+        "apiProperties": {
+          "statisticsEnabled": true
+        }
+      }
+    }
+  ]
+}
