bf6325bbf125dafa03dbd3866563fe29dc2580ae
abhiniveshmitra <133777817+abhiniveshmitra@users.noreply.github.com>
2025-06-26 11:00:44 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,1 +1,46 @@
-az deployment group create --resource-group rg-abhinivesh.mitra-1469 --template-file "C:\Users\e819643\Documents\azure-ai-resources.json" --parameters searchServiceName=mysearchservice foundryWorkspaceName=myfoundryws location=westeurope
+{
+  "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {
+    "searchServiceName": {
+      "type": "string",
+      "defaultValue": "mysearchservice"
+    },
+    "searchSku": {
+      "type": "string",
+      "defaultValue": "standard"
+    },
+    "foundryWorkspaceName": {
+      "type": "string",
+      "defaultValue": "myfoundryws"
+    },
+    "location": {
+      "type": "string",
+      "defaultValue": "westeurope"
+    }
+  },
+  "resources": [
+    {
+      "type": "Microsoft.Search/searchServices",
+      "apiVersion": "2023-11-01",
+      "name": "[parameters('searchServiceName')]",
+      "location": "[parameters('location')]",
+      "sku": {
+        "name": "[parameters('searchSku')]"
+      },
+      "properties": {
+        "partitionCount": 1,
+        "replicaCount": 1,
+        "hostingMode": "default",
+        "publicNetworkAccess": "Disabled"
+      }
+    },
+    {
+      "type": "Microsoft.AzureAI.Foundry/workspaces",
+      "apiVersion": "2024-05-01-preview",
+      "name": "[parameters('foundryWorkspaceName')]",
+      "location": "[parameters('location')]",
+      "properties": {}
+    }
+  ]
+}
