ab78449de4c41397a9365cb1398d4b5cc1e7b3cb
abhiniveshmitra <133777817+abhiniveshmitra@users.noreply.github.com>
2025-06-26 12:04:34 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,36 +1,52 @@
 {
   "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
   "contentVersion": "1.0.0.0",
-  "parameters": {
-    "accountName": {
-      "type": "string",
-      "defaultValue": "eus-cdr-openai"
-    },
-    "location": {
-      "type": "string",
-      "defaultValue": "westeurope"
-    },
-    "skuName": {
-      "type": "string",
-      "defaultValue": "S0"
-    }
-  },
+  "parameters": {},
+  "variables": {},
   "resources": [
     {
       "type": "Microsoft.CognitiveServices/accounts",
       "apiVersion": "2023-05-01",
-      "name": "[parameters('accountName')]",
-      "location": "[parameters('location')]",
+      "name": "eus-cdr-openai",
+      "location": "westeurope",
       "kind": "OpenAI",
       "sku": {
-        "name": "[parameters('skuName')]"
+        "name": "S0"
       },
       "properties": {
-        "apiProperties": {
-          "statisticsEnabled": true
-        }
+        "customSubDomainName": "eus-cdr-openai"
+      }
+    },
+    {
+      "type": "Microsoft.CognitiveServices/accounts/deployments",
+      "apiVersion": "2023-05-01",
+      "name": "eus-cdr-openai/gpt-4",
+      "dependsOn": [
+        "[resourceId('Microsoft.CognitiveServices/accounts', 'eus-cdr-openai')]"
+      ],
+      "properties": {
+        "model": {
+          "format": "OpenAI",
+          "name": "gpt-4",
+          "version": "1106-Preview"
+        },
+        "raiPolicyName": "Microsoft.Default"
+      },
+      "sku": {
+        "name": "Standard",
+        "capacity": 10
       }
     }
-  ]
+  ],
+  "outputs": {
+    "openaiEndpoint": {
+      "type": "string",
+      "value": "[reference(resourceId('Microsoft.CognitiveServices/accounts', 'eus-cdr-openai')).endpoint]"
+    },
+    "deploymentName": {
+      "type": "string",
+      "value": "gpt-4"
+    }
+  }
 }
-az deployment group create --resource-group rg-abhinivesh.mitra-1469 --template-file "C:\Users\t819643\Documents\openai.json"
+az deployment group create --resource-group abhinivesh.mitra-1469 --template-file Documents\openai.json
