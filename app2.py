ba94ba41dab09c42742dce05d39a657b6a3dde0c
abhiniveshmitra <133777817+abhiniveshmitra@users.noreply.github.com>
2025-06-26 11:49:21 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,1 +1,36 @@
-az provider list --query "[?contains(namespace, 'Azure')]" --output table
+{
+  "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {
+    "accountName": {
+      "type": "string",
+      "defaultValue": "eus-cdr-openai"
+    },
+    "location": {
+      "type": "string",
+      "defaultValue": "westeurope"
+    },
+    "skuName": {
+      "type": "string",
+      "defaultValue": "S0"
+    }
+  },
+  "resources": [
+    {
+      "type": "Microsoft.CognitiveServices/accounts",
+      "apiVersion": "2023-05-01",
+      "name": "[parameters('accountName')]",
+      "location": "[parameters('location')]",
+      "kind": "OpenAI",
+      "sku": {
+        "name": "[parameters('skuName')]"
+      },
+      "properties": {
+        "apiProperties": {
+          "statisticsEnabled": true
+        }
+      }
+    }
+  ]
+}
+az deployment group create --resource-group rg-abhinivesh.mitra-1469 --template-file "C:\Users\t819643\Documents\openai.json"
