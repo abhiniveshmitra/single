8a8158e9f5ff48ec4d5fa7d1cf6b74b53842e53b
abhiniveshmitra <133777817+abhiniveshmitra@users.noreply.github.com>
2025-06-26 10:50:14 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,8 +1,1 @@
-az account set --subscription 3936386c-bb2d-43ec-b7f1-2729bd65fcc0
-
-az deployment group create `
-  --resource-group rg-abhinivesh.mitra-1469 `
-  --template-file "C:\Users\e819643\Documents\azure-ai-resources.json" `
-  --parameters searchServiceName=mysearchservice `
-               foundryWorkspaceName=myfoundryws `
-               location=westeurope
+az deployment group create --resource-group rg-abhinivesh.mitra-1469 --template-file "C:\Users\e819643\Documents\azure-ai-resources.json" --parameters searchServiceName=mysearchservice foundryWorkspaceName=myfoundryws location=westeurope
