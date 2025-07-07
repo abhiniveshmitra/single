62b2f378272f808e091b46749741372e111323ee
abhiniveshmitra <133777817+abhiniveshmitra@users.noreply.github.com>
2025-07-07 15:16:27 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,6 +1,5 @@
 {
   "search": "*",
-  "filter": "organizer_user_id eq '5f9e4e6f-9f5e-4ecf-9f92-393d4cf13fd3'",
-  "select": "organizer_user_displayName, participants_user_id, participants_user_displayName",
-  "count": true
+  "select": "organizer_user_id, organizer_user_displayName, participants_user_id, participants_user_displayName",
+  "top": 5
 }
